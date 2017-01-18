5a8d37b8b0984af741ae7014236a58a9f69cef77
colorworld <563880188@qq.com>
2017-01-18 22:08:23 +0800

    第一个版本的简历

--- js/jianli.js
+++ js/jianli.js
@@ -8,15 +8,16 @@ var divs = $(".tab-content");
 $(function () {
     $(".uls li").on("click", function () {
         for (var i = 0; i < lis.length; i++) {
+
             lis[i].id = i;
             str = "div." + lis[i].id;
             $(str).hide();
             str = "";
-
+            ids = "#" + i;//取得 id
+            $(ids).removeClass("blue");//移除所以li列表的背景
         }
-        var  str2= "div." + this.id;
-        $(str2).slideDown(500);
-
-
+        $(this).addClass("blue");
+        var str2 = "div." + this.id;
+        $(str2).slideDown(1300);
     })
 });
